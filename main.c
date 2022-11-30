77ff0b3aaf776c32286d05a075cf1afac499bdb9
Jose López <jolopez@redsis.com>
2022-11-30 08:55:17 -0500

    New files added

--- main.c
+++ main.c
@@ -43,4 +43,7 @@ document.getElementById("btn-primary").innerHTML = "Hello World";
 document.getElementById("btn-primary").innerHTML = "Hello World";
 document.getElementById("btn-primary").innerHTML = "Hello World";
 document.getElementById("btn-primary").innerHTML = "Hello World";
+document.getElementById("btn-primary").innerHTML = "Hello World";
+document.getElementById("btn-primary").innerHTML = "Hello World";
+document.getElementById("btn-primary").innerHTML = "Hello World";
 
