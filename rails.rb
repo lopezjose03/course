33ee3a693ab5a38eb1ef5051ff05ebb6a47b3d0c
Jose López <jolopez@redsis.com>
2022-11-30 08:49:48 -0500

    Added web ruby file

---
+++ rails.rb
@@ -0,0 +1,21 @@
+a = 24
+b = 24
+c = a + b
+res = "el resultado es: ", c
+
+
+a = 24
+b = 24
+c = a + b
+res = "el resultado es: ", c
+
+a = 24
+b = 24
+c = a + b
+res = "el resultado es: ", c
+
+a = 24
+b = 24
+c = a + b
+res = "el resultado es: ", c
+
